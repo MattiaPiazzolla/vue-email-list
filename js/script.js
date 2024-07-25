90f43ad1c1bcdb63b46a8d026208fb2cec7ecba4
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-25 17:50:09 +0200

    [UPD] called the api using the library axios

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ createApp({
         generateTenRandomEmails(){
             // definisco un ciclo per iterare la chiamata che gerea l'email 10 volte 
             for (let i = 0; i < 10 ; i++){
-                
+                // utilizzo la libreria axios per chiamare l'api
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             }
         }
     }

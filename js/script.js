b0fb96eb23c31fcbb88f4a3652e726b1afd23cbc
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-25 17:44:05 +0200

    [UPD] defined a function that will generate 10 random emails

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ const { createApp } = Vue;
 
 createApp({
     data(){
-
+        return{
+            // definisco l'array dove andro ad inserire gli indirizzi email generati
+            randomEmails: null,
+        }
     },
     methods: {
-        
+        // definisco la funzione per generare l'email
+        generateTenRandomEmails(){
+
+        }
     }
 })

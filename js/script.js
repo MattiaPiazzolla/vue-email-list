29c6c283380540f7cb4f157c839c52c05331b9c0
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-25 18:10:30 +0200

    [UPD] completed

--- js/script.js
+++ js/script.js
@@ -5,12 +5,13 @@ createApp({
     data(){
         return{
             // definisco l'array dove andro ad inserire gli indirizzi email generati
-            randomEmails: null,
+            randomEmails: [],
         }
     },
     methods: {
         // definisco la funzione per generare l'email
         generateTenRandomEmails(){
+            
             // definisco un ciclo per iterare la chiamata che gerea l'email 10 volte 
             for (let i = 0; i < 10 ; i++){
                 // utilizzo la libreria axios per chiamare l'api
@@ -22,4 +23,4 @@ createApp({
             }
         }
     }
-})
+}).mount('#app')
